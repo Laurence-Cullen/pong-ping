96a2582dfb0df5bcb62c08b8e18c58a16d9817b4
Laurence Cullen <laurencesimoncullen@gmail.com>
2018-11-09 16:42:21 +0000

    cleaning up auth ui

--- public/scripts/firebase_setup.js
+++ public/scripts/firebase_setup.js
@@ -21,19 +21,6 @@ db.settings({
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-        // List of OAuth providers supported.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    ],
-    // Other config options...
-});
-
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
 var uiConfig = {
     callbacks: {
         signInSuccessWithAuthResult: function(authResult, redirectUrl) {
