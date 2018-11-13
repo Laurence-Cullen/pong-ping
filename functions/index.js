886b5239f1cf114b993aee0f19e36d5851dba169
Laurence Cullen <laurencesimoncullen@gmail.com>
2018-11-13 00:06:24 +0000

    charge500 functional, trying to diagnose Checkout button failure in index

--- functions/index.js
+++ functions/index.js
@@ -8,12 +8,11 @@ var stripe = require("stripe")("sk_test_x4FUCBx7GzOwM9j9RBtoH0lU");
 //  response.send("Hello from Firebase!");
 // });
 
-// Set your secret key: remember to change this to your live secret key in production
-// See your keys here: https://dashboard.stripe.com/account/apikeys
+// Charge user £5
 exports.charge500 = functions.https.onRequest((request, response) => {
 
-// Token is created using Checkout or Elements!
-// Get the payment token ID submitted by the form:
+    // Token is created using Checkout or Elements!
+    // Get the payment token ID submitted by the form:
     const token = request.body.stripeToken; // Using Express
 
     const charge = stripe.charges.create({
@@ -22,6 +21,6 @@ exports.charge500 = functions.https.onRequest((request, response) => {
         description: 'Example charge',
         source: token,
     }).then(function () {
-        return response.redirect('/');
+        return response.redirect('https://pong-ping1.firebaseapp.com/');
     });
 });
