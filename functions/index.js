e9ef49601675d974006356598ca7b40116367f4c
Laurence Cullen <laurencesimoncullen@gmail.com>
2018-11-14 17:04:08 +0000

    CORS handled in cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,26 @@
 const functions = require('firebase-functions');
 var stripe = require("stripe")("sk_test_x4FUCBx7GzOwM9j9RBtoH0lU");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 // Charge user £5
 exports.charge500 = functions.https.onRequest((request, response) => {
 
+    // boilerplate CORS code
+    response.set('Access-Control-Allow-Origin', '*');
+
+    if (request.method === 'OPTIONS') {
+        // Send response to OPTIONS requests
+        response.set('Access-Control-Allow-Methods', 'GET');
+        response.set('Access-Control-Allow-Headers', 'Content-Type');
+        response.set('Access-Control-Max-Age', '3600');
+        response.status(204).send('');
+    } else {
+        // Set CORS headers for the main request
+        response.set('Access-Control-Allow-Origin', '*');
+    }
+
     // Token is created using Checkout or Elements!
     // Get the payment token ID submitted by the form:
-    const token = request.body.stripeToken; // Using Express
+    const token = request.body.token; // Using Express
 
     const charge = stripe.charges.create({
         amount: 500,
@@ -21,6 +28,8 @@ exports.charge500 = functions.https.onRequest((request, response) => {
         description: 'Example charge',
         source: token,
     }).then(function () {
-        return response.redirect('https://pong-ping1.firebaseapp.com/');
+        return response.send('payment completed successfully');
+    }).catch(function (error) {
+        return response.send('encountered error: ' + error);
     });
 });
